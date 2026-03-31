1f8bdc253b095252818d4ed5c1120cd41b17d6eb
auyu0408 <datealive102164@gmail.com>
2026-03-31 17:59:20 +0800

    add ReadIn: Read decision file and change attribute

---
+++ llvm/include/llvm/Transforms/Utils/ReadIn.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "llvm/Transforms/Utils/MyFunction.h"
+
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Metadata.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Support/CommandLine.h"
+
+namespace llvm {
+    class ReadInPass : public PassInfoMixin<ReadInPass> {
+        public:
+            PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+    };
+}
